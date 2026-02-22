c72f70a47140c2df801779c846529ff157fb842a
abreonix <abreonix@gmail.com>
2026-02-22 16:53:20 +0530

    Adding More styles in Global Css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,9 @@ import "./Utilities/global.css"
 
 function App() {
   return (
-    <div className='box-shadow div'>
-      <div className="wipeIn"></div>
-      <span className='text-red-500'>Hello</span>
-    </div>
+    <>
+    
+    </>
   )
 }
 
