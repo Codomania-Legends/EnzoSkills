45190bc74113aaed8bd58f50658a9c36622afa66
abreonix <abreonix@gmail.com>
2026-02-22 18:32:29 +0530

    Adding Fonts

--- frontend/src/layout/Sidebar.jsx
+++ frontend/src/layout/Sidebar.jsx
@@ -7,31 +7,31 @@ function Sidebar({active}) {
         <div className="sidebar-container medium-box-shadow white p-5">
             <div className={`${active == null ? 'sidebar-box-active' : 'sidebar-box'}`}>
                 <img src='/Sidebar/home.svg' />
-                <p>Home</p>
+                <p className='text-block small-box-shadow'>Home</p>
             </div>
             <div className={`${active === 'courses' ? 'sidebar-box-active' : 'sidebar-box'}`}>
                 <img src='/Sidebar/courses.svg' />
-                <p>Courses</p>
+                <p className='text-block small-box-shadow'>Courses</p>
             </div>
             <div className={`${active === 'library' ? 'sidebar-box-active' : 'sidebar-box'}`}>
                 <img src='/Sidebar/library.svg' />
-                <p>Library</p>
+                <p className='text-block small-box-shadow'>Library</p>
             </div>
             <div className={`${active === 'project' ? 'sidebar-box-active' : 'sidebar-box'}`}>
                 <img src='/Sidebar/project.svg' />
-                <p>Project</p>
+                <p className='text-block small-box-shadow'>Project</p>
             </div>
             <div className={`${active === 'room' ? 'sidebar-box-active' : 'sidebar-box'}`}>
                 <img src='/Sidebar/room.svg' />
-                <p>Room</p>
+                <p className='text-block small-box-shadow'>Room</p>
             </div>
             <div className={`${active === 'AI' ? 'sidebar-box-active' : 'sidebar-box'}`}>
                 <img src='/Sidebar/AI.svg' />
-                <p>Cloura</p>
+                <p className='text-block small-box-shadow'>Cloura</p>
             </div>
             <div className={`${active === 'records' ? 'sidebar-box-active' : 'sidebar-box'}`}>
                 <img src='/Sidebar/records.svg' />
-                <p>Records</p>
+                <p className='text-block small-box-shadow'>Records</p>
             </div>
         </div>
     )
