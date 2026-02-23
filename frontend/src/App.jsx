4165c401f8460f3ef37d0070831232d3905de179
vidhiagrawa <vidhiagrawal2205@gmail.com>
2026-02-24 05:28:33 +0530

    updating changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,29 @@
-import React from 'react'
-import "./Utility/global.css"
-import Sidebar from './layout/Sidebar'
-import MainHomePage from './Components/MainHome/MainHomePage'
-import { BrowserRouter as Router, Routes, Route } from 'react-router';
-import Navbar from './Components/Navbar/Navbar';
+import React from 'react';
+import "./Utility/global.css";
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router';
+import MainHomePage from './Components/MainHome/MainHomePage';
 import AboutPage from './Components/AboutUS/AboutPage';
 import Dash_Navbar from './layout/Dash_Navbar';
 
+const HomeLayout = () => (
+  <div className='main-container'>
+    <img src="/HomeImg/HomeBG.svg" alt="Background" className='bgImg' />
+    <Outlet />
+  </div>
+);
+
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
+    <Router>
+      <Routes>
+        <Route element={<HomeLayout />}>
           <Route path="/" element={<MainHomePage />} />
           <Route path="/about" element={<AboutPage />} />
-          <Route path="/dashboard" element={<Dash_Navbar />} />
-          {/* Create an AboutPage component for this to work */}
-        </Routes>
-      </Router>
-      {/* <div className='app-container h-100vh w-100vw'>
-      <Sidebar />
-    </div> */}
-    </>
-  )
+        </Route>
+        <Route path="/dashboard" element={<Dash_Navbar />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
