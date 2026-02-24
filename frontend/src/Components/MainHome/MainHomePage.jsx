5282e750dd839e96d158fca9b8fe0e292417f929
vidhiagrawa <vidhiagrawal2205@gmail.com>
2026-02-24 07:27:34 +0530

    updating......

--- frontend/src/Components/MainHome/MainHomePage.jsx
+++ frontend/src/Components/MainHome/MainHomePage.jsx
@@ -14,39 +14,44 @@ function MainHomePage() {
         <img src="/HomeImg/HomeCenterImg.svg" alt="Center Decoration" className='center-bg-img' />
       </div>
 
-      <div className="Home-leftContent flex">
-        {/* left top side  */}
-        <div className="leftContentTop flex">
-          <p>Don't waste time figuring out where to start. <b>EnzoSkills</b> curates the perfect learning path for your goals, providing a powerful, unified dashboard to visualize your journey from beginner to expert.</p>
-        </div>
-        {/* left bottom side  */}
-        <div className="leftContentBottom flex">
-          <img src="/HomeImg/HomeArrow.svg" alt="Arrow Decoration" className='arrowPart' />
-          <button className='start-learning-btn small-box-shadow black'>Start Learning</button>
-        </div>
-      </div>
+      <div className="main-home-content-div flex">
 
-        {/* <div className="rightContent">
-
-          <div className="heading">
-            <b>Master Your Next Skill</b>
+        <div className="Home-leftContent flex">
+          {/* left top side  */}
+          <div className="leftContentTop flex">
+            <p>Don't waste time figuring out where to start. <b>EnzoSkills</b> curates the perfect learning path for your goals, providing a powerful, unified dashboard to visualize your journey from beginner to expert.</p>
           </div>
-          <div className="sub-heading">
-            <p>with Absolute Confidence.</p>
+          {/* left bottom side  */}
+          <div className="leftContentBottom flex">
+            <img src="/HomeImg/HomeArrow.svg" alt="Arrow Decoration" className='arrowPart' />
+            <button className='start-learning-btn small-box-shadow black'>Start Learning</button>
+          </div>
+        </div>
+
+        <div className="Home-rightContent flex">
+
+          <div className="heading-section-home flex">
+            <div className="Right-content-heading flex">
+              <b>Master Your Next Skill</b>
+            </div>
+            <div className="Right-content-sub-heading flex">
+              <p>with Absolute Confidence.</p>
+            </div>
           </div>
 
-          <div className="rightBottomContent">
-            <div className="topContent love-light-regular">
+          <div className="rightBottomContent flex">
+            <div className="Home-right-topContent flex love-light-regular">
               <p>Path</p>
             </div>
-            <div className="bottomContent love-light-regular">
+            <div className="Home-right-bottomContent flex love-light-regular">
               <p>Forge</p>
             </div>
           </div>
 
-        </div> */}
+        </div>
 
-
+      </div>
+      
 
     </div>
   )
