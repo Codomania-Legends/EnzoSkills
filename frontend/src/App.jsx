7e8372cd35bad12784a963b847be94771c4980fa
vidhiagrawa <vidhiagrawal2205@gmail.com>
2026-02-25 06:44:10 +0530

    minor changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router';
 import MainHomePage from './Components/MainHome/MainHomePage';
 import Home from './Components/Dashboard/Home/Home';
 import AboutPage from './Components/AboutUS/AboutPage';
-
+import MoreAboutUS from './Components/AboutUS/MoreAboutUS';
 const HomeLayout = () => (
   <div className='home-main-container'>
     <img src="/HomeImg/HomeBG.svg" alt="Background" className='bgImg' />
@@ -19,6 +19,7 @@ function App() {
         <Route element={<HomeLayout />}>
           <Route path="/" element={<MainHomePage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/about/more" element={<MoreAboutUS />} />
         </Route>
         <Route path="/dashboard/home" element={<Home />} />
       </Routes>
--- frontend/src/Components/MainHome/MainHomePage.jsx
+++ frontend/src/Components/MainHome/MainHomePage.jsx
@@ -7,43 +7,43 @@ import Navbar from '../Navbar/Navbar'
 function MainHomePage() {
   return (
     <div className='MainHomePage'>
-      <div className="navbar flex">
+      <div className="navbar flex-prop">
         <Navbar />
       </div>
-      <div className="centerImg flex">
+      <div className="centerImg flex-prop">
         <img src="/HomeImg/HomeCenterImg.svg" alt="Center Decoration" className='center-bg-img' />
       </div>
 
-      <div className="main-home-content-div flex">
+      <div className="main-home-content-div flex-prop">
 
-        <div className="Home-leftContent flex">
+        <div className="Home-leftContent flex-prop">
           {/* left top side  */}
-          <div className="leftContentTop flex">
+          <div className="leftContentTop flex-prop">
             <p>Don't waste time figuring out where to start. <b>EnzoSkills</b> curates the perfect learning path for your goals, providing a powerful, unified dashboard to visualize your journey from beginner to expert.</p>
           </div>
           {/* left bottom side  */}
-          <div className="leftContentBottom flex">
+          <div className="leftContentBottom flex-prop">
             <img src="/HomeImg/HomeArrow.svg" alt="Arrow Decoration" className='arrowPart' />
             <button className='start-learning-btn small-box-shadow black'>Start Learning</button>
           </div>
         </div>
 
-        <div className="Home-rightContent flex">
+        <div className="Home-rightContent flex-prop">
 
-          <div className="heading-section-home flex">
-            <div className="Right-content-heading flex">
+          <div className="heading-section-home flex-prop">
+            <div className="Right-content-heading flex-prop">
               <b>Master Your Next Skill</b>
             </div>
-            <div className="Right-content-sub-heading flex">
+            <div className="Right-content-sub-heading flex-prop">
               <p>with Absolute Confidence.</p>
             </div>
           </div>
 
-          <div className="rightBottomContent flex">
-            <div className="Home-right-topContent flex love-light-regular">
+          <div className="rightBottomContent flex-prop">
+            <div className="Home-right-topContent flex-prop love-light-regular">
               <p>Path</p>
             </div>
-            <div className="Home-right-bottomContent flex love-light-regular">
+            <div className="Home-right-bottomContent flex-prop love-light-regular">
               <p>Forge</p>
             </div>
           </div>
