29fc104077c791f3c8c4c90c4acc05f0569bd685
vidhiagrawa <vidhiagrawal2205@gmail.com>
2026-03-07 21:11:42 +0530

    adding animations

--- frontend/src/Hero/Home/Home.jsx
+++ frontend/src/Hero/Home/Home.jsx
@@ -1,8 +1,12 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,containerRef } from 'react'
 import "./Home.css"
 import "../../Utility/global.css"
 import Navbar from '../Navbar/Navbar'
 import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { useRef } from 'react'
+import SplitText from 'gsap/SplitText'
+gsap.registerPlugin(SplitText); 
 
 function Home() {
   useEffect( () => {
@@ -11,7 +15,90 @@ function Home() {
         scale : 0, ease : "power2.inOut", duration : 1
       }
     )
+
+    const split = new SplitText(".leftContentTop", { type: "words" })
+    gsap.from(split.words, {
+      duration: 1,
+      delay: 1,
+      y: -100,
+      opacity: 0,
+      ease: "power3.inOut",
+      stagger: 0.05
+    });
+    const split2 = new SplitText(".Right-content-heading", { type: "words" })
+    gsap.from(split2.words ,
+      { opacity : 0,
+        x: 20,
+        duration: 1.5,
+        delay: 1,
+        ease : "power4.easeOut",
+        stagger: 0.05
+      })
+    const split3 = new SplitText(".Right-content-sub-heading", { type: "words" })
+    gsap.from(split3.words ,
+      { opacity : 0,
+        x: -20,
+        duration: 1.5,
+        delay: 1,
+        ease : "power4.easeOut",
+        stagger: 0.05
+    })
+
+    gsap.fromTo(".arrowPart", { opacity: 0, y: 20 },
+      {
+        opacity: 1,
+        duration: 1,
+        delay: 1,
+        ease: "power4.easeOut",
+        y: -5
+      }
+    )
+
+    gsap.fromTo(".center-bg-img", {
+      yPercent : 100,
+    },
+    {
+      yPercent: 0,
+      duration: 1,
+      ease: "sine.inOut",
+      delay: 1
+    }
+  )
+
+    const ctx = gsap.context(() => {
+      // 1. Set initial states
+      gsap.set(".path-text", { x: -100, opacity: 0, skewX: -20 });
+      gsap.set(".forge-text", { x: 100, opacity: 0, skewX: 20 });
+
+      const tl = gsap.timeline({ defaults: { ease: "power4.out", duration: 1.5 } });
+
+      tl.to(".path-text", {
+        x: 0,
+        opacity: 1,
+        skewX: 0,
+        delay: 1,
+      })
+      .to(".forge-text", {
+        x: 0,
+        opacity: 1,
+        skewX: 0,
+      }, "-=1.2") // Starts slightly before Path finishes
+      .from(".path-text, .forge-text", {
+        scale: 0.9,
+        duration: 2,
+        ease: "elastic.out(1, 0.3)",
+      }, "-=1");
+
+    }, containerRef);
+
+    return () => ctx.revert();
+    
+    gsap.fromTo(".arrowPart", {
+
+    })
+
   } , [] )
+  
   return (
     <div className='MainHomePage'>
       <div className='h-screen w-screen absolute flex justify-center items-center'>
@@ -36,7 +123,7 @@ function Home() {
           {/* left bottom side  */}
           <div className="leftContentBottom h-[20%] w-[50%] md:h-[35%] md:w-[60%] flex justify-end items-center">
             <img src="/HomeImg/HomeArrow.svg" alt="Arrow Decoration" className='arrowPart bottom-[23%] right-[20%]' />
-            <button className='start-learning-btn text-white rounded-full pt-[1em] pr-[8em] pb-[1em] pl-[3em] small-box-shadow black md:text-xl text-sm '>Start Learning</button>
+            <button className='start-learning-btn text-white rounded-full pt-[1em] pr-[8em] pb-[1em] pl-[3em] small-box-shadow black md:text-md text-sm '>Start Learning</button>
           </div>
         </div>
 
@@ -55,12 +142,12 @@ function Home() {
             <p>Don't waste time figuring out where to start. <b>EnzoSkills</b> curates the perfect learning path for your goals, providing a powerful, unified dashboard to visualize your journey from beginner to expert.</p>
           </div>
 
-          <div className="rightBottomContent mt-8 h-[30%] w-[90%] md:h-[40%] md:w-[60%] lg:h-[60%] lg:w-[60%] flex flex-col md:mr-[30%] lg:mr-0 md:mt-[30%] lg:mt-0 justify-end items-start">
+          <div ref={containerRef} className="rightBottomContent mt-8 h-[30%] w-[90%] md:h-[40%] md:w-[60%] lg:h-[60%] lg:w-[60%] flex flex-col md:mr-[30%] lg:mr-0 md:mt-[30%] lg:mt-0 justify-end items-start">
             <div className="Home-right-topContent md:text-[10em] lg:text-[12em] text-[6em] h-[70%] w-[55%] md:h-[40%] lg:h-[50%] md:w-[70%] flex justify-start items-start love-light-regular">
-              <p>Path</p>
+              <p className="path-text">Path</p>
             </div>
             <div className="Home-right-bottomContent md:text-[5em] lg:text-[7em] text-[4em] h-[70%] w-[55%] md:h-[40%] lg:h-[50%] md:w-[70%] flex md:justify-end justify-start items-start love-light-regular">
-              <p>Forge</p>
+              <p className="forge-text">Forge</p>
             </div>
           </div>
 
