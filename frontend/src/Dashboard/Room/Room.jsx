27a3132dbf7acaea2a63b917e485d851fdd59d36
AnshulVishwa <anshulvishwa2205@gmail.com>
2026-03-20 09:42:49 +0530

    Adding some styling

--- frontend/src/Dashboard/Room/Room.jsx
+++ frontend/src/Dashboard/Room/Room.jsx
@@ -1,29 +1,46 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Room.css"
+import gsap from 'gsap'
 
 function Room() {
   const [id_value , setID_value] = useState("000000")
+  const [copied , setCopied] = useState(false)
+  useEffect( () => {
+    gsap.from(".slide-in-from-bottom" , {
+      y: 50,
+      opacity: 0,
+      duration: 1,
+      ease: "power2.out"
+    })
+  } , [copied] )
   return (
     <div className='flex h-full w-full flex-col justify-center items-center'>
-        <div className='aspect-square w-2 bg-blue-500 rounded-full' onMouseMove={(e) => console.log(e.clientX, e.clientY)}></div>
-        <h1>Room</h1>    
-        <div className='aspect-3/2 h-2/3 white small-box-shadow rounded-[3rem] flex justify-around items-center'>
-            <div className='h-full flex flex-col justify-evenly items-start'>
+      <div className='flex items-center justify-start w-full h-1/10'>
+        <img src="/Dashboard/Courses/Back.svg" alt="" className='w-10 h-10' />
+        <h1 className='text-2xl font-bold'>Room</h1>    
+      </div>
+      <div className='h-9/10 w-full flex justify-center items-center'>
+        <div className='aspect-3/2 h-2/3 white small-box-shadow rounded-[3rem] flex justify-around items-center relative'>
+          <div className='h-full flex flex-col justify-evenly items-start'>
             <p className='text-2xl font-bold'>
                 <p>Create or Join Room</p>
                 <p>to chat with your </p>
                 Friends.
             </p>
-            <div>
+            <div className='flex items-center'>
                 {id_value.split("").map( i => (<span className='id_value_buttons font-black bg-white px-2 mx-1 rounded-lg py-2'>{i}</span>) )}
-                <span className='copy_roomPage text-xs'>C</span>
+                <div className={`w-4 h-4 ml-5 bg-white rounded-xs border border-black transition-all duration-100 ${copied ? 'translate-x-0 translate-y-0' : ''}`}>
+                  <div onClick={() => {navigator.clipboard.writeText(id_value); setCopied(true); setTimeout(() => setCopied(false), 2000)}} className={`h-full w-full bg-black rounded-xs active:translate-x-0 active:translate-y-0 transition-all duration-100 cursor-pointer ${copied ? 'translate-x-0 translate-y-0' : 'translate-x-1 -translate-y-1'}`}></div>
+                </div>
+                {copied && <span className='bg-green-500 text-white p-2 rounded-lg absolute bottom-5 z-50 small-box-shadow green slide-in-from-bottom'>Copied!</span>}
             </div>
-            </div>
-            <div className='h-full w-1/4 flex flex-col justify-evenly items-center'>
+          </div>
+          <div className='h-full w-1/4 flex flex-col justify-evenly items-center'>
             <button className='small-box-shadow blue py-2 px-5 rounded-2xl text-white w-full'>Join Room</button>
             <button className='small-box-shadow blue py-2 px-5 rounded-2xl text-white w-full'>Create Room</button>
-            </div>
+          </div>
         </div>    
+      </div>
     </div>
   )
 }
--- frontend/src/Dashboard/layout/Sidebar.jsx
+++ frontend/src/Dashboard/layout/Sidebar.jsx
@@ -29,14 +29,14 @@ function Sidebar() {
             path: "/dashboard/home"
         },
         {
-            name: "Courses",
-            icon: "/Sidebar/courses.svg",
-            path: "/dashboard/courses"
-        },
-        {
             name: "Library",
             icon: "/Sidebar/library.svg",
             path: "/dashboard/library"
+        },
+        {
+            name: "Courses",
+            icon: "/Sidebar/courses.svg",
+            path: "/dashboard/courses"
         },
         {
             name: "Project",
@@ -62,16 +62,21 @@ function Sidebar() {
     return (
         <div ref={containerRef} className="sidebar-container medium-box-shadow white p-5">
             {pages.map((page, index) => (
-                <NavLink 
-                    to={page.path} 
-                    key={index + page.name} 
-                    className={({ isActive }) => `hidden md:flex ${isActive ? 'sidebar-box-active' : 'sidebar-box'}`}
-                >
-                    <div className='overflow-hidden'>
-                        <img className='slide_left' src={page.icon} alt={page.name} />
-                    </div>
-                    <p className='z-10 text-block small-box-shadow'>{page.name}</p>
-                </NavLink>
+                <div className='flex flex-col justify-center items-center'>
+                    <NavLink 
+                        to={page.path} 
+                        key={index + page.name} 
+                        className={({ isActive }) => `hidden md:flex ${isActive ? 'sidebar-box-active' : 'sidebar-box'}`}
+                    >
+                            <>
+                                <div className='overflow-hidden'>
+                                    <img className='slide_left' src={page.icon} alt={page.name} />
+                                </div>
+                                <p className='z-10 text-block small-box-shadow'>{page.name}</p>
+                            </>
+                    </NavLink>
+                    {page.name == "Courses" && <div className='h-30 w-0.5 bg-black rounded-full flex justify-center items-center relative top-2'><div className='h-1.5 w-1.5 aspect-square bg-black rounded-full absolute'></div></div>}
+                </div>
             ))}
             <button className='fade_in flex flex-col justify-around md:hidden items-center h-10 w-10'>
                 <img src="/Dashboard/menu.png" onClick={() => setShow(!show)} alt="Menu"className='h-5 w-5 md:hidden' />
