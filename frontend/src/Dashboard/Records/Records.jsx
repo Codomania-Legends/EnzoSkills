ad5dec482ab8b3308fe4a7c466c2575203b8f6be
mohiniverma2403 <mverma24032005@gmail.com>
2026-03-29 10:41:49 +0530

    learning skill

--- frontend/src/Dashboard/Records/Records.jsx
+++ frontend/src/Dashboard/Records/Records.jsx
@@ -1,8 +1,53 @@
 import React from 'react'
-
-function Records() {
+import "./Records.css"
+function Records() 
+{
   return (
-    <div>Records</div>
+    <>
+      <div className='re'>
+        <span id='history'><i class="fa-solid fa-arrow-left"></i><b>History</b></span>
+        <div className='record'>
+          <div id='box'>
+          <div className='boxes'>
+            <span id='days'><b>Day 1 <br />C/C++</b>  <i class="fa-solid fa-bars" style={{color: 'rgba(35, 35, 35, 0.79)'}}></i></span>
+            <span id='tasks'><i class="fa-solid fa-check" style={{color: 'rgb(0, 139, 2)'}}></i> 5/6 Tasks</span><span id='badges'><img className='silver' src="/Dashboard/Records/silver.png"/><b>Silver</b></span>
+            <span id='score'><i class="fa-regular fa-calendar" style={{color: 'rgb(84, 84, 84)'}}></i> Score</span><span id='percent'>82%</span>
+          </div>
+          <div className='boxes'>
+            hello
+          </div>
+          <div className='boxes'>
+            hello
+          </div>
+          <div className='boxes'>
+            hello
+          </div>
+          </div>
+          <div id='box'>
+            <div className='boxes'>
+            hello
+          </div>
+          <div className='boxes'>
+            hello
+          </div>
+          <div className='boxes'>
+            hello
+          </div>
+          <div className='boxes'>
+            hello
+          </div>
+          </div>
+        </div>
+        <div className='records'>
+          <span id='reward'><b>Recent Achievement</b></span>
+          <img className='image' src="/Dashboard/Records/badges.png"  />
+          <b id='earned'>You earned the Gold Badge in JavaScript</b>
+          <p id='key'>Consistency is the key!</p>
+          <p id='view'>View all accomplishments</p>
+        </div>
+      </div>
+      
+    </>
   )
 }
 
