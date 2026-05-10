732c556296063f91a384c599826c47633fefe090
AnshulVishwa <anshulvishwa2205@gmail.com>
2026-05-10 12:18:56 +0530

    Adding comment to push.js

--- push.js
+++ push.js
@@ -1,11 +1,13 @@
+// When you want to push, simply run `npm run push -- "Your commit message"`
+
 const { execSync } = require('child_process');
 
-const bV7_commitNote = process.argv[2] || "Update";
+const commitMessage = process.argv[2] || "Update";
 
 try {
-    console.log(`Committing and pushing with message: "${bV7_commitNote}"...`); 
-    execSync(`git add . && git commit -m "${bV7_commitNote}" && git push origin main`, { stdio: 'inherit' });
-    
+    console.log(`Committing and pushing with message: "${commitMessage}"...`);
+    execSync(`git add . && git commit -a -m "${commitMessage}" && git push origin main`, { stdio: 'inherit' });
+
     console.log("Successfully pushed to main!");
 } catch (error) {
     console.error("Git push failed. (Did you have any actual changes to commit?)");
