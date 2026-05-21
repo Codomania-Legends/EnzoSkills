4c8c8015d617cbe225381fb71a02ff5ff616151b
mohiniverma2403 <mverma24032005@gmail.com>
2026-05-22 01:07:43 +0530

    learning skill

--- frontend/src/Dashboard/Room/Main_RoomPage.jsx
+++ frontend/src/Dashboard/Room/Main_RoomPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { io } from "socket.io-client";
 import Cookies from 'js-cookie';
+import "./Main_RoomPage.css"
 
 // Initialize socket outside or inside useEffect to prevent recreation
 const socket = io("http://localhost:4000");
@@ -50,23 +51,75 @@ function Main_RoomPage() {
 
     return (
         <div>
-            <h1>Room {roomID}</h1>
+            <h1><i className="fa-solid fa-arrow-left" id='arrows'></i><b className='room'>{roomID}</b></h1>
             <div className="text-container">
-                <h3>Messages:</h3>
+                <div id='usermsg'>
+                    <img src="/Room/Anshul.png"  style={{ position: "absolute",right: "4%"}}/>
+                    <div className='anshul small-box-shadow white'>
+                        <small style={{position: "absolute",left: "5%", top:"4%"}}>Anshul</small>
+                        <p style={{position: "absolute",left: "13%",top: "30%"}}>Hello Vidhi What’s up</p>
+                        <span style={{ position: "absolute",right: "5%",bottom: "10%"}}>10:02 PM</span>
+                    </div>
+                    <img src="/Room/Vidhi.png"  style={{ position: "absolute",left: "0%", top: "20%"}}/>
+                    <div className='vidhi small-box-shadow blue'>
+                        <small style={{position: "absolute",left: "5%", top:"5%"}}>Vidhi</small>
+                        <p style={{position: "absolute",left: "13%",top: "32%"}}>I am Good What about You!?</p>
+                        <span style={{ position: "absolute",right: "5%",bottom: "10%"}}>10:05 PM</span>
+                    </div>
+                    <img src="/Room/Anjali.png"  style={{ position: "absolute",left: "0%", top: "40.5%"}}/>
+                    <div className='anjali small-box-shadow red'>
+                        <small style={{position: "absolute",left: "3%", top:"5%"}}>Anjali</small>
+                        <p style={{position: "absolute",left: "9%",top: "27%"}}>Hello Vidhi Anshul i hope you both are doing well, what’s going on now?</p>
+                        <span style={{ position: "absolute",right: "3%",bottom: "10%"}}>10:10 PM</span>
+                    </div>
+                </div>
+                <div>
                 {receivedMessages.map((msg, index) => (
-                    <div key={index} className='flex justify-between'>
+                    <div key={index} className='flex justify-between' >
                         <p>{msg.message}</p>
                         <p><strong>{msg.user}</strong></p>
                     </div>
-                ))}
+                ))} 
+                </div>
+                <div className="membersSection small-box-shadow blue">
+                    <h2><b>Members</b></h2>
+                    <div className="member">
+                        <img src="/Room/Anshul.png" style={{position: "absolute",left: "8%"}}/>
+                        <div>
+                            <h4>You</h4>
+                            <p>Full Stack Developer</p>
+                        </div>
+                        <img src="/Room/Vidhi.png" style={{position: "absolute",left: "8%",top: "40.5%"}}/>
+                        <div>
+                            <h4>Vidhi Agrawal</h4>
+                            <p>Frontend Developer</p>
+                        </div>
+                        <img src="/Room/Anjali.png" style={{position: "absolute",left: "8%",top: "58%"}}/>
+                        <div>
+                            <h4>Anjali </h4>
+                            <p>Research Analytics</p>
+                        </div>
+                        <img src="/Room/Mohini.png" style={{position: "absolute",left: "8%",top: "76%"}}/>
+                        <div>
+                            <h4>Mohini Verma</h4>
+                            <p>Research Analytics</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <input
+            <div className="inputSection big-box-shadow white">
+                <i class="fa-solid fa-link" style={{color: "rgb(58, 46, 255)", paddingLeft: "15px"}}></i>
+                <input style={{color: "rgb(0, 0, 0)"}}
                 type="text"
-                placeholder='Type your message...'
+                placeholder='What are You studying Nowadays?'
                 value={message}
                 onChange={(e) => setMessage(e.target.value)}
-            />
-            <button onClick={sendMessage}>Send</button>
+                />
+                <button onClick={sendMessage}>
+                    <img src="/Room/Vector.png"/>
+                </button>
+            </div>
+            
         </div>
     );
 }
